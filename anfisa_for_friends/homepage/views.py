2cdc517abe23dcacff0f9680177c23ec3766b401
rommaz <rommaz@localhost.localdomain>
2023-05-18 13:33:01 +0500

    Django debug toolbar installed

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from ice_cream.models import IceCream
+from django.db.models import Q
 
+
+#def index(request):
+#   template = 'homepage/index.html'
+#    return render(request, template)
 
 def index(request):
     template = 'homepage/index.html'
-    return render(request, template)
+    # Запишите в переменную ice_cream_list новый QuerySet
+    ice_cream_list = IceCream.objects.values('id', 'title', 'description'
+    # Верни только те объекты, у которых в поле is_on_main указано True:
+        ).filter(# Делаем запрос, объединяя два условия
+        # через Q-объекты и оператор AND:
+        Q(is_published=True) & (Q(is_on_main=True) | Q(description__contains='икра')))
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    return render(request, template, context)
